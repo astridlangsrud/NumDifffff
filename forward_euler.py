4f1411958dccd20cac36f287d800e531a38bf264
William Hornslien <w.hornslien@gmail.com>
2018-03-05 17:29:41 +0100

    Astrid sa jeg skulle pushe

--- forward_euler.py
+++ forward_euler.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-h = 0.037
-k = 0.0001
+h = 0.5
+k = 0.01
 L = 10
 x = np.linspace(-L/2,L/2,int(L/h)+1)
 sigma = 0.054
-tau = 30
+tau = 1/120
 V_0 = 120
 rho_hat = 120
 E = 100
@@ -17,10 +17,7 @@ f_rmp = 121
 rho_up = 20
 
 def q(t):
-    if t<10:
-        return 15
-    else:
-        return 0
+    return 121*3600
 
 def phi(x):
     return 2*np.pi*(sigma**2)*np.exp(-(x**2)/(2*(sigma**2)))
@@ -38,16 +35,16 @@ def f_u(U,m):
     u2 = U[1,m]*((U[1,m+1]-U[1,m])/h)
     return np.array([u1,u2])
 
-u = np.zeros([2,len(x)])
+u = np.zeros([2,len(x)]) #2 x M
 u_next = np.zeros([2,len(x)])
 initial_velocity = V_ro(rho_up)
 u[0,:] = rho_up
 u[1,:] = initial_velocity
-for n in range(1000):
-    #print(u)
+for n in range(10):
     for m in range(len(x)-1):
         b_next = b(u,m,n)
         f_next = f_u(u,m)
+        print(u[0,1:10])
         u_next[0,m] = u[0,m]+k*(b_next[0]-f_next[0])
         u_next[1, m] = u[1,m]+k*(b_next[1]-f_next[1])
         u = u_next
