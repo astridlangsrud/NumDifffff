3638ec38dac7c075510aa82527d4e116f45b4369
Julie Wilberg <juliemwi@stud.ntnu.no>
2018-03-06 10:03:26 +0100

    lax-friedrich

--- lax_friedrichs.py
+++ lax_friedrichs.py
@@ -25,14 +25,14 @@ def phi(x):
 def V_ro(ro):
     return V_0*(1-(ro/rho_hat))/(1+E*((ro/rho_hat)**4))
 
-def b(U,m,n):
+def s(U,m,n):
     u1 = q(n*k)*phi(m*h)
-    u2 = ((V_ro(U[0,m])-U[1,m])/tau)-(((c_0**2)*((U[0,m+1]-U[0,m])/h))-mu*(U[1,m+1]-2*U[1,m]+U[1,m-1])/(h*h))/U[0,m]
+    u2 = ((V_ro(U[0,m])-U[1,m])/tau)
     return np.array([u1, u2])
 
 def f_u(U,m):
-    u1 = ((U[0,m+1]-U[0,m])/h)*U[1,m]+((U[1,m+1]-U[1,m])/h)*U[0,m]
-    u2 = U[1,m]*((U[1,m+1]-U[1,m])/h)
+    u1 = U[0,m]*U[1,m]
+    u2 = 1/2*U[1,m]**2 + c_0**2*np.log(U[0,m])
     return np.array([u1,u2])
 
 u = np.zeros([2,len(x)]) #2 x M
@@ -41,11 +41,14 @@ initial_velocity = V_ro(rho_up)
 u[0,:] = rho_up
 u[1,:] = initial_velocity
 for n in range(10):
-    for m in range(len(x)-1):
-        b_next = b(u,m,n)
+    u[:,len(x)] = u[:,len(x)-1]
+    for m in range(1,len(x)-1):
+        s_next = s(u,m,n)
         f_next = f_u(u,m)
+        f_next_m1 = f_u(u,m-1) #m-1
+        f_next_p1 = f_u(u,m+1) #m+1
         #print(u[0,1:10])
-        u_next[0,m] = u[0,m]+k*(b_next[0]-f_next[0])
-        u_next[1, m] = u[1,m]+k*(b_next[1]-f_next[1])
-        u = u_next
+        u_next[0,m] = (u[0,m-1]+ u[0,m+1])/2 -k/(2*h)*(f_next_p1[0]-f_next_m1[0])+k*s_next[0]
+        u_next[1,m] = (u[1,m-1]+ u[1,m+1])/2 -k/(2*h)*(f_next_p1[1]-f_next_m1[1])+k*s_next[1]
+    u = u_next
 print(u)
